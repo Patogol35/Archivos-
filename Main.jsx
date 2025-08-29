cdf2e12a32a6e5883ee68a7b2d9102dfb50863d8
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-28 22:04:09 -0500

    Update Main.jsx

--- Main.jsx
+++ Main.jsx
@@ -7,6 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* Contenedor global de notificaciones */}
     <ToastContainer position="top-right" autoClose={3000} />
   </React.StrictMode>
 );
