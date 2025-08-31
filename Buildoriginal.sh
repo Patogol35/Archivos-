c0c4919c89deff7bad69a454a0f406c698d8c0ad
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-31 01:53:04 -0500

    Create Buildoriginal.sh

---
+++ Buildoriginal.sh
@@ -0,0 +1,13 @@
+
+
+#!/usr/bin/env bash
+set -o errexit
+
+# Instala dependencias
+pip install -r requirements.txt
+
+# Archivos estáticos
+python manage.py collectstatic --noinput
+
+# Migraciones
+python manage.py migrate
